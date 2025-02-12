75038be404e075768ec7672add68e6a764f95226
jalkothadia <jalkothadia2217@gmail.com>
2025-02-12 18:44:41 +0530

    lab4A4

--- lab4A4.java
+++ lab4A4.java
@@ -1,1 +1,15 @@
-public class lab4A4
+import java.util.Scanner;
+
+public class lab4A4{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String str=sc.nextLine();
+
+		System.out.println("length of String is="+str.length());
+
+		for(int i=0;i<str.length()/2;i++){
+			System.out.print(str.charAt(i));
+		}
+	}
+}
